b36987af08bfea4c1dbb228ea030cf1043d32f62
Shelli Martinez <shelli.mrtnz@gmail.com>
2023-10-17 19:30:12 -0400

    add completed lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+const copyOfCats = [...cats];
+
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+    return cats;
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+    return cats;
+  }
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+  }
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+  }
+
+function appendCat() {
+    return [...cats, "Broom"];
+  }
+
+function prependCat() {
+    return ["Arnold", ...cats];
+  }
+
+function removeLastCat() {
+    copyOfCats.pop();
+    return copyOfCats;
+  }
+
+function removeFirstCat() {
+    return cats.slice(1);
+  }
